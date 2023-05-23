8c87bb02800d9ac4bc5af2d89950b1baff7ecf04
סולומון ברייב <solomonb@ordaprint.local>
2023-05-23 17:52:24 +0300

    add ant design components

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,21 +1,25 @@
-import React from "react";
-import { useForm } from "../contexts/FormContext";
-
-export default function FileUpload({ name, label }) {
-  const { handleFileUpload } = useForm();
-
-  return (
-    <>
-      <label className="form__label form__label_file" htmlFor={name}>
-        {label}
-      </label>
-      <input
-        type="file"
-        accept=".jpg, .jpeg, .png, .gif, .bmp, .svg"
-        name={name}
-        id={name}
-        onChange={handleFileUpload}
-      />
-    </>
-  );
-}
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, message, Upload } from "antd";
+const props = {
+  name: "file",
+  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+  headers: {
+    authorization: "authorization-text",
+  },
+  onChange(info) {
+    if (info.file.status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+const App = () => (
+  <Upload {...props}>
+    <Button icon={<UploadOutlined />}>Click to Upload</Button>
+  </Upload>
+);
+export default App;
