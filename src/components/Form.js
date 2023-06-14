b6672821e2239a106af8101fd393f688f2d2d8f1
סולומון ברייב <solomonb@ordaprint.local>
2023-06-14 10:15:33 +0300

    adding logic to load with first accordion open and only one open at a time

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,6 +17,7 @@ let { categories: cats, colors: cols, sizes: siz } = data;
 cats = cats.map((cat) => cat.name);
 
 const Form = () => {
+  const [expanded, setExpanded] = useState(0);
   const [categories, setCategories] = useState(cats);
   const [colors, setColors] = useState(cols);
   const [sizes, setSizes] = useState(siz);
@@ -30,84 +31,110 @@ const Form = () => {
     addItem,
     removeItem,
     totalPrice,
-    isTelError,
-    telValidationMess,
   } = useForm();
+
+  const addItemAndExpand = () => {
+    addItem();
+    setExpanded(itemsIds.length);
+  };
 
   return (
     <form className="form">
-      {itemsIds.map((el, i) => (
-        <div>
-          <Accordion>
-            <AccordionSummary
-              //expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
+      <div className="form__products-container">
+        {itemsIds.map((el, i) => (
+          <div className="form__accordion">
+            <Accordion
+              sx={{
+                "box-shadow": "none",
+              }}
+              expanded={expanded === i}
+              onClick={() => setExpanded(i)}
             >
-              <Typography>הדפסה {i + 1}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                <section
-                  item-index={el}
-                  key={el}
-                  className="form__section form__section_type_product"
+              <AccordionSummary
+                //expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                sx={{
+                  cursor: "pointer",
+                  width: "100%",
+                  textAlign: "center",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Typography
+                  sx={{
+                    margin: "0 auto",
+                    fontSize: "1.1em",
+                    fontWeight: "bold",
+                  }}
                 >
-                  <h2 className="form__product-title">הדפסה {i + 1}</h2>
-                  <div className="form__group">
-                    <div className="form__dropdowns">
-                      <Dropdown
-                        list={categories}
-                        name="category"
-                        label="סוג חולצה"
-                        formValues={itemValues[i]?.category}
-                        handleChange={(e) => handleItemsChange(e, el)}
-                      />
-                      <Dropdown
-                        list={colors}
-                        name="color"
-                        label="צבע"
-                        formValues={itemValues[i]?.color}
-                        handleChange={(event) => handleItemsChange(event, el)}
-                      />
-                      <Dropdown
-                        list={sizes}
-                        name="size"
-                        label="מידה"
-                        formValues={itemValues[i]?.size}
+                  הדפסה {i + 1}
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  <section
+                    item-index={el}
+                    key={el}
+                    className="form__section form__section_type_product"
+                  >
+                    <div className="form__group">
+                      <div className="form__dropdowns">
+                        <Dropdown
+                          list={categories}
+                          name="category"
+                          label="סוג חולצה"
+                          formValues={itemValues[i]?.category}
+                          handleChange={(e) => handleItemsChange(e, el)}
+                        />
+                        <Dropdown
+                          list={colors}
+                          name="color"
+                          label="צבע"
+                          formValues={itemValues[i]?.color}
+                          handleChange={(event) => handleItemsChange(event, el)}
+                        />
+                        <Dropdown
+                          list={sizes}
+                          name="size"
+                          label="מידה"
+                          formValues={itemValues[i]?.size}
+                          handleChange={(event) => handleItemsChange(event, el)}
+                        />
+                      </div>
+                      <Input
+                        name="amount"
+                        label="כמות"
+                        formValues={itemValues[i]?.amount}
                         handleChange={(event) => handleItemsChange(event, el)}
                       />
                     </div>
-                    <Input
-                      name="amount"
-                      label="כמות"
-                      formValues={itemValues[i]?.amount}
-                      handleChange={(event) => handleItemsChange(event, el)}
-                    />
-                  </div>
-                  <div className="form__group form__group_type_images">
-                    <FileUpload name="image1" label="הדפס קידמי" />
-                    <FileUpload name="image2" label="הדפס אחורי" />
-                  </div>
-                  {itemsIds.length == 1 ? null : (
-                    <button
-                      type="button"
-                      className="form__remove-item-btn"
-                      onClick={() => removeItem(el)}
-                    >
-                      <BsFillTrash3Fill />
-                    </button>
-                  )}
-                </section>
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </div>
-      ))}
+                    <div className="form__group form__group_type_images">
+                      <FileUpload name="image1" label="הדפס קידמי" />
+                      <FileUpload name="image2" label="הדפס אחורי" />
+                    </div>
+                    {itemsIds.length == 1 ? null : (
+                      <button
+                        type="button"
+                        className="form__remove-item-btn"
+                        onClick={() => removeItem(el)}
+                      >
+                        <BsFillTrash3Fill />
+                      </button>
+                    )}
+                  </section>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        ))}
+      </div>
       <button
         type="button"
         className="btn btn-primary form__add-btn"
-        onClick={() => addItem()}
+        onClick={() => addItemAndExpand()}
       >
         הוסף הדפסה <AiOutlinePlusCircle />
       </button>
