9291343bb47a59ddb02addd198739d4181b3bf3e
סולומון ברייב <solomonb@ordaprint.local>
2023-07-17 14:11:42 +0300

    debuggin scroll

--- src/components/Form.js
+++ src/components/Form.js
@@ -66,7 +66,7 @@ const Form = () => {
       // const yOffset = -50;
       // const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
       // setTimeout(() => window.scrollTo({ top: y, behavior: "smooth" }), 1000);
-      element?.scrollIntoView({ behavior: "smooth" });
+      element?.scrollIntoView({ behavior: "smooth", block: "start" });
     }
     setIndex(index + 1);
   }, [expanded]);
