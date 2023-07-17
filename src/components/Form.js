85d033779b38a3a822dcb10499abbbf0c1ca80cb
סולומון ברייב <solomonb@ordaprint.local>
2023-07-17 13:36:46 +0300

    try scrollTo to scroll item to viewport when added

--- src/components/Form.js
+++ src/components/Form.js
@@ -63,11 +63,15 @@ const Form = () => {
     if (index != 0) {
       const element = document.getElementById(expanded);
       console.log(element);
-      element?.scrollIntoView({
-        behavior: "auto",
-        block: "center",
-        inline: "center",
-      });
+      // element?.scrollIntoView({
+      //   behavior: "auto",
+      //   block: "center",
+      //   inline: "center",
+      // });
+      const yOffset = -100;
+      const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
+
+      window.scrollTo({ top: y, behavior: "smooth" });
     }
     setIndex(index + 1);
   }, [expanded]);
